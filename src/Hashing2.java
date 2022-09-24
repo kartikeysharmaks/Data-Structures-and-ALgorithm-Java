bc4fc56adee3ddd9f545e68613bc308cc21c56fe
kartikeysharmaks <kartikeysharma1616@gmail.com>
2022-09-24 13:17:42 +0530

    HashSet

---
+++ src/Hashing2.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Hashing2 {
+    public static void main(String[] args) {
+
+        //declaration and initialization of Hashmap
+        HashMap<String,Integer> map = new HashMap<>();
+
+        //put method to add key value pairs in the map
+        map.put("China",150);
+        map.put("India", 130);
+        map.put("US",50);
+
+        //to print elements and its value inside the map
+        System.out.println(map);
+
+        //get method to get a particular element and its value by its key
+        int ans = map.get("China");
+        System.out.println(ans);
+
+        //remove method to remove element by key
+        map.remove("US");
+        System.out.println(map);
+
+        //size method to get the size of the map
+        System.out.println(map.size());
+    }
+}
